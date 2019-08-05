c4bef9b1236fc62e6a08c755dabbae0da8fb89da
JDutil <jeff.dutil@juul.com>
2019-08-05 12:43:31 -0600

    Update Dependencies & Fix Rubocop Violations

--- spec/features/admin/order_comments_spec.rb
+++ spec/features/admin/order_comments_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.feature 'Order Comments', :js do
   let!(:order) { create(:completed_order_with_totals) }
 
@@ -7,14 +8,16 @@ RSpec.feature 'Order Comments', :js do
     login_as_admin
   end
 
-  it "adding comments" do
+  it 'adding comments' do
     visit spree.comments_admin_order_path(order)
     expect(page).to have_text(/No Comments found/i)
 
     fill_in 'Comment', with: 'A test comment.'
     click_button 'Add Comment'
 
-    expect(page).to have_text(/Comments \(1\)/i) # uppercase < v1.2, sentence case >= v1.3
+    # uppercase < v1.2, sentence case >= v1.3
+    expect(page).to have_text(/Comments \(1\)/i)
     expect(page).to have_text('A test comment.')
   end
 end
+# rubocop:enable Metrics/BlockLength
