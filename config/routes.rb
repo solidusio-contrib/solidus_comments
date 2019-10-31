e0ff0e52f96dc5b47c3b76a0c954218b536dade6
Alessandro Desantis <desa.alessandro@gmail.com>
2019-10-31 18:11:35 +0100

    Remove shipments controller and routes
    It doesn't make sense to decorate Spree::Api::ShipmentsController,
    since nothing calls the `comments` action there. This just looks
    like a bad attempt at re-adapting the extension after the
    Spree::Admin::ShipmentsController class was removed.
    
    Fixes #5.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
-
-#map.namespace :admin do |admin|
-#  admin.resources :comments
-#  admin.resources :comment_types
-#
-#  admin.resources :orders, :member => {:comments => :get} do |order|
-#    order.resources :shipments, :member => {:comments => :get}
-#  end
-#end
-#
-
 Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :comments, only: [:create, :update, :destroy]
@@ -18,15 +7,6 @@ Spree::Core::Engine.routes.draw do
       member do
         get :comments
       end
-
-      resources :shipments do
-        member do
-         get :comments
-       end
-      end
     end
   end
-
-#match '/admin/comments' => 'admin/comments', :via => [:get, :post]
-#  match '/admin/comment_types' => 'admin/comment_types', :via => [:get, :post]
 end
