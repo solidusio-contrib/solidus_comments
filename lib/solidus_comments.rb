0c25e7a61193d9579e3cca9c71efdb078eeebea7
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-01-17 14:34:27 +0100

    Upgrade to the latest solidus_dev_support
    Override some extension files with the new generated ones.

--- lib/solidus_comments.rb
+++ lib/solidus_comments.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-require 'spree_api'
-require 'spree_backend'
 require 'solidus_core'
 require 'solidus_support'
-require 'acts_as_commentable'
-require 'solidus_comments/engine'
+
 require 'deface'
+require 'acts_as_commentable'
+
+require 'solidus_comments/version'
+require 'solidus_comments/engine'
