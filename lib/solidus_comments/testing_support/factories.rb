0f477160a0b7215f641609b4b1f769aad660308f
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2021-09-07 08:47:18 +0200

    Add a simple factory for comments

--- lib/solidus_comments/testing_support/factories.rb
+++ lib/solidus_comments/testing_support/factories.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :comment, class: Spree::Comment do
+    association :commentable, factory: :order
+    user
+    sequence(:comment) { |n| "Comment #{n}" }
+  end
 end
