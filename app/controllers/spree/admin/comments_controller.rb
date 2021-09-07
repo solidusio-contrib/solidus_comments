b4f8e4b5de14f01c471f1368b2d74c1d35f76793
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2021-09-07 08:49:58 +0200

    Enhance system specs for the new validation

--- app/controllers/spree/admin/comments_controller.rb
+++ app/controllers/spree/admin/comments_controller.rb
@@ -5,6 +5,11 @@ module Spree
     class CommentsController < Spree::Admin::ResourceController
       private
 
+      def render_after_create_error
+        flash.keep
+        redirect_to request.referer
+      end
+
       def location_after_save
         request.referer
       end
